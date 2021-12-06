bdd8747cfb0a5545c5010d882e5e5f99f6c50d88
Andrew Pearce <89484534+Andr3wPearce@users.noreply.github.com>
2021-12-06 17:25:41 -0500

    Add files via upload

---
+++ hannukah.ps1
@@ -0,0 +1,33 @@
+
+$Day = 8
+echo "Happy Hannukah! According to the script, today is day $Day of Hannukah!"
+
+
+
+Write-Host "                       "-NoNewLine
+Write-Host " "-BackgroundColor Yellow #Shamash
+Write-Host "               "-NoNewLine
+
+if($Day -ge 1){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #1st
+Write-Host " "-NoNewLine
+if($Day -ge 2){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #2nd
+Write-Host " "-NoNewLine
+if($Day -ge 3){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #3rd
+Write-Host " "-NoNewLine
+if($Day -ge 4){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #4th
+Write-Host " | "-NoNewLine
+if($Day -ge 5){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #5th
+Write-Host " "-NoNewLine
+if($Day -ge 6){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #6th
+Write-Host " "-NoNewLine
+if($Day -ge 7){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #7th
+Write-Host " "-NoNewLine
+if($Day -ge 8){Write-Host " "-NoNewLine -BackgroundColor Yellow} else{Write-Host " "-NoNewLine} #8th
+Write-Host "" #new line
+Write-Host "               | | | | | | | | |              " 
+Write-Host "               \ | | | | | | | /              "
+Write-Host "                 \ | | | | | /                "
+Write-Host "                   \ | | | /                  "
+Write-Host "                     \ | /                    "
+Write-Host "                      |_|                     "
+Write-Host "                  ____|_|____                 "
